6e4610c2d42c7a575a8c302284471308ffabb44f
Beatriz de Luca Patriota <beatriz.lp@alumni.usp.br>
2023-09-05 22:49:56 -0300

    Changed index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,86 @@
-import React from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+"use client"
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
+import {
+  CarOutlined,
+  HomeOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme, Row } from 'antd';
 import Link from 'next/link'
+import { getCookie } from 'cookies-next'
+import { verifica } from '@/service/user';
+import { useRouter } from 'next/router';
+const { Header, Sider, Content } = Layout;
 
-const LoginPage= () => {
-  const onFinish = values => {
-    console.log('Received values of form: ', values);
-  };
+export default function Home() {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+const router = useRouter()
+
 
   return (
-    <div style={{ 
-      width: '100vw',
-height: '100vh',
-display: 'flex',
-flexDirection: 'row',
-justifyContent: 'center',
-alignItems: 'center'
-    }}>
-      
-    <Form
-      name="normal_login"
-      className="login-form"
-      style={{ width: '300px', height: '300px'}}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Por favor coloque seu username!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Por favor coloque sua senha!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
+    <Layout style={{ overflow: 'auto'}}>
+      <Sider trigger={null} collapsible collapsed={collapsed} style={{ height: '100vh'}}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'Perfil',
+            }
+          ]}
         />
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Login
-        </Button>
-         Ou <Link href="/cadastro"> se cadastre agora!</Link>
-      </Form.Item>
-    </Form>
-    </div>
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            height: '100%',
+            background: colorBgContainer,
+          }}
+        >
+          <Row>
+          <Image src='/renegociacao.png' width={1640} height={350}/>
+          <div style={{color: '#EC7000', width: '100%', padding:'60px', textAlign: 'center', backgroundColor: '#FAF7F5'}}>
+            <h1>Aproveite e renegocie suas dívidas com as ofertas do Itaú</h1>  
+          </div>
+          <div style={{color: 'black', width: '100%', padding: '60px', textAlign: 'center'}}>
+            <h1>Quais dívidas você gostaria de renegociar?</h1>
+            <div style={{marginTop: '20px'}}>
+            <Link href="/itau"><Button icon={<HomeOutlined />}>Dívidas do Itaú</Button></Link>
+            <Link href="https://renegocie.gruporecovery.com/registro?utm_medium=lprenegocie&utm_source=reneg&utm_campaign=renegitau"><Button icon={<CarOutlined />}>Outras dívidas</Button></Link>          
+            </div>
+          </div>
+          </Row>
+        </Content>
+      </Layout>
+    </Layout>
   );
-};
-
-export default LoginPage
+}
